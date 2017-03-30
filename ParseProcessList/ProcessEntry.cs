78e32529f18b2211dec341a1f0338af041b7aa41
David Ebbo <david.ebbo@gmail.com>
2017-03-30 09:43:55 -0700

    Init

---
+++ ParseProcessList/ProcessEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParseProcessList
+{
+    class ProcessEntry
+    {
+        public static ProcessEntry Parse(string line)
+        {
+            if (line.StartsWith("Pid")) return null;
+
+            string[] parts = line.Split(new char[] { ' ' }, options: StringSplitOptions.RemoveEmptyEntries);
+
+            var entry = new ProcessEntry();
+
+            entry.PID = parts[0];
+            entry.WorkingSet = Int32.Parse(parts[1]);
+            entry.HandleCount = Int32.Parse(parts[2]);
+            var timeParts = parts[3].Split(':');
+            entry.CpuTime = new TimeSpan(0, Int32.Parse(timeParts[0]), Int32.Parse(timeParts[1]), Int32.Parse(timeParts[2]));
+            entry.Exe = parts[4];
+            if (parts.Length > 5)
+            {
+                entry.Process = parts[5];
+            }
+
+            return entry;
+        }
+
+        public string PID { get; set; }
+        public int WorkingSet { get; set; }
+        public int HandleCount { get; set; }
+        public TimeSpan CpuTime { get; set; }
+        public TimeSpan CpuTimeDelta { get; set; }
+        public string Exe { get; set; }
+        public string Process { get; set; }
+    }
+}
---
+++ ParseProcessList/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ParseProcessList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Syntax: ParseProcessList file1 file2");
+                return;
+            }
+
+            var first = ParseFile(args[0]);
+            var second = ParseFile(args[1]);
+
+            foreach(var newEntry in second.Values)
+            {
+                if (first.TryGetValue(newEntry.PID, out ProcessEntry oldEntry))
+                {
+                    newEntry.CpuTimeDelta = newEntry.CpuTime - oldEntry.CpuTime;
+                }
+            }
+
+            foreach (var newEntry in second.Values.Where(e => e.CpuTimeDelta != TimeSpan.Zero).OrderByDescending(e => e.CpuTimeDelta))
+            {
+                Console.WriteLine($"{newEntry.PID,-12} {newEntry.Exe,-35} {newEntry.Process,-35}: {newEntry.CpuTimeDelta}");
+            }
+        }
+
+        static IDictionary<string, ProcessEntry> ParseFile(string file)
+        {
+            var dict = new Dictionary<string, ProcessEntry>();
+
+            foreach (var line in File.ReadAllLines(file))
+            {
+                ProcessEntry entry = ProcessEntry.Parse(line);
+                if (entry == null) continue;
+
+                dict[entry.PID] = entry;
+            }
+
+            return dict;
+        }
+    }
+}
