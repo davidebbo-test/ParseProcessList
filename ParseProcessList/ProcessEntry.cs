b19e6f0bb5521d9e596eb99c0c8ab42a0490c520
David Ebbo <david.ebbo@gmail.com>
2018-09-07 09:30:43 -0700

    Update column ordering

--- ParseProcessList/ProcessEntry.cs
+++ ParseProcessList/ProcessEntry.cs
@@ -16,13 +16,13 @@ namespace ParseProcessList
 
             entry.PID = parts[0];
             entry.WorkingSet = Int32.Parse(parts[1]);
-            entry.HandleCount = Int32.Parse(parts[2]);
-            var timeParts = parts[3].Split(':');
+            entry.HandleCount = Int32.Parse(parts[3]);
+            var timeParts = parts[4].Split(':');
             entry.CpuTime = new TimeSpan(0, Int32.Parse(timeParts[0]), Int32.Parse(timeParts[1]), Int32.Parse(timeParts[2]));
-            entry.Exe = parts[4];
-            if (parts.Length > 5)
+            entry.Exe = parts[5];
+            if (parts.Length > 6)
             {
-                entry.Process = parts[5];
+                entry.Process = parts[6];
             }
 
             return entry;
